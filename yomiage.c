aae542a9b634c282b3d1039fc1f78bb09edb4922
小さめのクレーンです。 <ssskaigp03@gmail.com>
2019-08-15 15:20:32 +0900

    behavior fix

--- yomiage.c
+++ yomiage.c
@@ -15,7 +15,7 @@ int main(){
 
     filename = (unsigned char *)malloc(sizeof(unsigned char)*1);
     if(filename == NULL){
-        printf("error\n");
+        printf("memory preparation error\n");
         exit(1);
     }else{
     }
@@ -23,10 +23,11 @@ int main(){
     printf("Please input data-path.\n");
     printf("-> ");
     for(int flug = 0;flug != 1;path_size++){
-            if(path_size < 1){
-                tmp = (unsigned char *)realloc(filename,sizeof(unsigned char)*path_size);
+            if(path_size >= 1){
+                tmp = (unsigned char *)realloc(filename,sizeof(unsigned char)*path_size + 9);
                 if(tmp == NULL){
-                    printf("error 10\n");
+                    printf("memory preparation error\n");
+                    printf("(%d bytes)\n",path_size);
                     free(filename);
                     exit(1);
                 }else{
@@ -44,19 +45,21 @@ int main(){
 
     }
     printf("data-path : %s\n",filename);
+    printf("path_size : %d\n",path_size - 1);
     /*ファイル名入力*/
     input = (unsigned char *)malloc(sizeof(unsigned char)*2);
     if(input == NULL){
-        printf("error1\n");
+        printf("memory preparation error\n");
         exit(1);
     }else{
     }
     printf("ready\n");
+    printf("Please input read sentence\n-> ");
     for(int flug = 0;flug != 1;i++){
         if(i>=1){
             tmp = (unsigned char *)realloc(input,sizeof(unsigned char)*(i+2));
             if(tmp == NULL){
-                printf("error2\n");
+                printf("memory preparation error\n");
                 free(filename);
                 exit(1);
             }else{
@@ -72,8 +75,8 @@ int main(){
         }else{
         }
     }
-    printf("%s\n",input);
-    printf("%d\n",i-1);
+    printf("read sentence : %s\n",input);
+    printf("%d bytes\n",i-1);
         /*ファイル読み込み・データチャンクコピー*/
     int mozisu = i-1;
     unsigned char output[4];
@@ -84,7 +87,6 @@ int main(){
     unsigned char data_bit[2];
     unsigned int data_size = 0;
     unsigned char buff;
-
     for(i = 0;i < mozisu;i = i+2){
         output[0] = input[i];
         output[1] = input[i+1];
@@ -113,10 +115,10 @@ int main(){
             filename[path_size + 6] = '\0';
         }
         FILE *size_s;
-        printf("%s:",filename);
+        printf("%s : ",filename);
         size_s = fopen(filename,"rb");
         if(size_s == NULL){
-            printf("\nfile loading error\n");
+            printf("file loading error\n");
             free(input);
             free(filename);
             exit(1);
@@ -130,7 +132,7 @@ int main(){
 
     output_data = (unsigned char *)malloc(sizeof(unsigned char)*output_size + 44);
     if(output_data == NULL){
-        printf("error3\n");
+        printf("memory preparation error\n");
         free(input);
         free(filename);
         exit(1);
@@ -181,16 +183,16 @@ int main(){
         if(first_flag == 0){
             fseek(Rwav,22,SEEK_CUR);
             fread(channels,sizeof(unsigned char),2,Rwav);
-            printf("channel:%d%d\n",channels[0],channels[1]);
+            //printf("channel(data):%d%d\n",channels[0],channels[1]);
             fread(sample_rate,sizeof(unsigned char),4,Rwav);
-            printf("sampling rate:%d%d%d%d\n",sample_rate[0],sample_rate[1],sample_rate[2],sample_rate[3]);
+            //printf("sampling rate:%d%d%d%d\n",sample_rate[0],sample_rate[1],sample_rate[2],sample_rate[3]);
             fread(bps,sizeof(unsigned char),4,Rwav);
-            printf("BPS:%d%d%d%d\n",bps[0],bps[1],bps[2],bps[3]);
+            //printf("BPS:%d%d%d%d\n",bps[0],bps[1],bps[2],bps[3]);
             fread(data_set,sizeof(unsigned char),2,Rwav);
-            printf("data align:%d%d\n",data_set[0],data_set[1]);
+            //printf("data align:%d%d\n",data_set[0],data_set[1]);
             fread(data_bit,sizeof(unsigned char),2,Rwav);
-            printf("bit depth:%d%d\n",data_bit[0],data_bit[1]);
-            printf("-------------------------------------------------\n");
+            //printf("bit depth:%d%d\n",data_bit[0],data_bit[1]);
+            //printf("-------------------------------------------------\n");
             first_flag = 1;
         }else{
         }
@@ -199,7 +201,7 @@ int main(){
         int test = 1;
         test = fread(&data_size,sizeof(unsigned int),1,Rwav);
         if(test == 0){
-            printf("error4.1\n");
+            printf("file reading error\n");
             free(input);
             free(filename);
             free(output_data);
@@ -213,13 +215,13 @@ int main(){
             copy++;
         }
         fclose(Rwav);
-        printf("%d character\n",i);
+        //printf("%d character\n",i);
     }
     /*ファイル名作成*/
     unsigned char *wavname;
     wavname = (unsigned char *)malloc(sizeof(unsigned char)*(mozisu + 5));
     if(wavname == NULL){
-        printf("error5\n");
+        printf("memory preparation error\n");
         free(input);
         free(filename);
         free(output_data);
@@ -236,21 +238,64 @@ int main(){
     wavname[mozisu + 4] = '\0';
     printf("%s\n",wavname);
     unsigned char *W_address;
-    unsigned char W_path[] = {'C',':','\\','U','s','e','r','s','\\','s','s','s','k','a','i','\\','M','u','s','i','c','\\','d','a','t','a','\\','O','U','T','P','U','T','\\'};
-    W_address = (unsigned char *)malloc(sizeof(unsigned char)*(mozisu + 39));
-    if(W_address == NULL){
-        printf("error6\n");
+    unsigned char *W_path;
+
+    W_path = (unsigned char *)malloc(sizeof(unsigned char)*1);
+    if(W_path == NULL){
+        printf("memory preparation error\n");
         free(input);
         free(filename);
         free(output_data);
+        free(W_path);
         exit(1);
     }else{
     }
-    for(i = 0;i < 34;i++){
+
+    printf("Please input path to output folder\n-> ");
+    i = 0;
+    for(int flug=0 ; flug != 1 ; i++){
+        if(i>=1){
+            tmp = (unsigned char *)realloc(W_path,sizeof(unsigned char) * i + 1);
+            if(tmp == NULL){
+                printf("memory preparation error\n");
+                free(filename);
+                free(input);
+                free(output_data);
+                free(W_path);
+                exit(1);
+            }else{
+            }
+            W_path = tmp;
+        }else{
+        }
+        W_path[i] = getchar();
+        //printf("%d %c\n",i,W_path[i]);
+        if(W_path[i] == '\n'){
+            W_path[i] = '\\';
+            flug = 1;
+        }else{
+        }
+    }
+
+    int W_path_size = i;
+    printf("output folder path : %s\n",W_path);
+    printf("path size : %d\n",W_path_size);
+
+    W_address = (unsigned char *)malloc(sizeof(unsigned char)*(mozisu + 4 + W_path_size));
+    if(W_address == NULL){
+        printf("memory preparation error\n");
+        free(input);
+        free(filename);
+        free(output_data);
+        free(W_path);
+        exit(1);
+    }else{
+    }
+    for(i = 0 ; i < W_path_size ; i++){
         W_address[i] = W_path[i];
     }
-    for(i = 0;i < mozisu + 5;i++){
-        W_address[i+34] = wavname[i];
+    for(i = 0 ; i < mozisu + 4 ; i++){
+        W_address[i + W_path_size] = wavname[i];
     }
     printf("%s\n",W_address);
     /*ヘッダ作成*/
@@ -312,6 +357,7 @@ int main(){
         free(output_data);
         free(wavname);
         free(W_address);
+        free(W_path);
         fclose(Wwav);
         exit(1);
     }else{
@@ -327,5 +373,6 @@ int main(){
     free(output_data);
     free(wavname);
     free(W_address);
+    free(W_path);
     return 0;
 }
